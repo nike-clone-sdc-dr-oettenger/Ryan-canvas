8ecb1a679f34a5808ac07dc43aad3e3a81771641
Richard Kang <richardkang1121@gmail.com>
2019-09-17 00:10:17 -0500

    Add modal and adjust localhost to ec2 instance

--- client/src/canvas.jsx
+++ client/src/canvas.jsx
@@ -1,27 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
-import Table from './modules/table.jsx'
+import Table from './modules/table.jsx';
+import FullSize from './modules/imageList.jsx';
+import Modal from 'react-modal';
+
+
+Modal.setAppElement('#canvas');
+
+const customStyle = {
+  content: {
+    top: '1px',
+    left: '1px',
+    right: '1px',
+    bottom: '1px'
+  }
+}
 
 class Canvas extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      images: []
-    }
+      images: [],
+      modalIsOpen: false
+    };
+
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
   }
 
   componentDidMount () {
-    $.get('http://localhost:1121/api/images', {shoe_id: 0}, (data) => {
+    $.get('http://18.207.197.100:1121/api/images', {shoe_id: 0}, (data) => {
       this.setState({
         images: data
       })
     })
   }
 
+  openModal () {
+    this.setState({modalIsOpen: true});
+  }
+
+  closeModal () {
+    this.setState({modalIsOpen: false});
+  }
+
   render () {
     return (<div className='imgCanvas'>
-      <Table images={this.state.images}/>
+      <Table images={this.state.images} onClick={this.openModal}/>
+      <Modal style={customStyle} isOpen={this.state.modalIsOpen} images={this.state.images}>
+        <FullSize images={this.state.images} onClick={this.closeModal} />
+      </Modal>
     </div>)
   }
 };
