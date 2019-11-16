1b5fe893519f8198548fe0eb9774e5789b267d85
mille424 <mille424@gmail.com>
2019-11-16 08:31:13 -0800

    worked on readme

--- database/login.config.js
+++ database/login.config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   psql: {
-    host: 'ec2-18-237-133-166.us-west-2.compute.amazonaws.com',
+    host: 'ec2-18-236-181-36.us-west-2.compute.amazonaws.com',
     user: 'power_user',
     password: 'password',
     database: 'nike_canvas',
